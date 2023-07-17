b5947bb2c06622e953bd8b73e1703655a56b8491
Joonas Kemppainen <karmakebu@gmail.com>
2023-07-17 13:00:53 +0300

    fix

--- bloglist/backend/index.js
+++ bloglist/backend/index.js
@@ -2,6 +2,7 @@ const {info} = require("./utils/logger")
 const {PORT} = require("./utils/config")
 const app = require("./app")
 
+
 app.listen(PORT, () => {
 	info(`Server running on port ${PORT}`)
 })
