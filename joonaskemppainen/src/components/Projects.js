5ab222da05cb371e8bc3556df6f2b27b19f2c9ad
Joonas Kemppainen <karmakebu@gmail.com>
2023-07-20 12:31:33 +0300

    jammming added

--- joonaskemppainen/src/components/Projects.js
+++ joonaskemppainen/src/components/Projects.js
@@ -1,6 +1,7 @@
 import blogapp from "../resources/blogapp.png"
 import weatherapp from "../resources/weatherapp.png"
 import calculatorapp from "../resources/calculatorapp.png"
+import jammmingapp from "../resources/jammmingapp.png"
 
 function Projects() {
     return (
@@ -25,6 +26,28 @@ function Projects() {
                         </div>
                         <br />
                         <p>My blog app, developed as part of the Full Stack Open course, combines the power of React for the frontend, Node.js with Express for the backend, and MongoDB for the database. It allows you to create, like, and comment on blogs.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="projects-container">
+                <div className="project-container">
+                    <div className="project-image">
+                        <img src={jammmingapp} alt="laptop" />
+                    </div>
+                    <div className="project-info">
+                        <h3>Jammming app</h3>
+                        <div className="link-left">
+                            <a href="https://jammming-kwyq.onrender.com/" target="_blank" rel="noreferrer">
+                                <p className="live">Live</p>
+                            </a>
+                        </div>
+                        <div className="link-left">
+                            <a href="https://github.com/JoonasKemppainen/jammming" target="_blank" rel="noreferrer">
+                                <p className="github">Github</p>
+                            </a>
+                        </div>
+                        <br />
+                        <p>My jammming app, crafted with React and the Spotify API, used to create your own custom playlists on Spotify. This app allows you to search for songs, and effortlessly add your favorite tracks to your personalized playlists. developed as part of the Codecademy course.</p>
                     </div>
                 </div>
             </div>
